0cc556b3f17b348f60e5b3818884932fc102d422
afsoonAlizadeh <afsoon.alizadeh1991@gmail.com>
2023-01-15 14:04:14 +0330

    create router for dashboard

---
+++ src/app/dashboard/router/dashboard-router.ts
@@ -0,0 +1,16 @@
+import { EditCustomerComponent } from './../customer/component/edit-customer/edit-customer.component';
+import { AddCustomerComponent } from './../customer/component/add-customer/add-customer.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ViewCustomerComponent } from '../customer/component/view-customer/view-customer.component';
+const routes: Routes = [
+  { path: '', component: ViewCustomerComponent },
+  { path: 'add', component: AddCustomerComponent },
+  { path: 'edit/:id', component: EditCustomerComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardRouter {}
